5fa91046065c58a267c4fa23d5da9500c769e131
Sarang-Admin <sarang.management.team@gmail.com>
2023-11-28 20:09:05 +0530

    Student profile completed

--- app/admin/studentManagement/[id]/page.jsx
+++ app/admin/studentManagement/[id]/page.jsx
@@ -17,249 +17,238 @@ const StudentProfilePage = async ({ params }) => {
           <Image src={student.img || "/images/noavatar.png"} alt="" fill />
         </div>
         <div className="mt-5 text-3xl ml-5">
-          {student.lastname} {student.firstname}
-        </div>{" "}
-        <br />
-        <div className="mt-5 text-xl ml-5">{student.studentID}</div>{" "}
+          {student.firstname} {student.lastname}
+        </div>
       </div>
       <div className={styles.formContainer}>
         <form action={updateStudent} className={styles.form}>
-          <Input
-            type=""
-            label="વિદ્યાર્થીનો યુનિક આઈડી"
-            name="studentID"
-            className="mt-8 w-[30%] mb-8 text-gray-900"
-            placeholder={student.studentID}
-          />
-          <Input
-            type=""
-            label="જી.આર. નંબર"
-            name="grNumber"
-            className="mt-8 w-[30%] mb-8 text-gray-900"
-            placeholder={student.grNumber}
-          />
-          <Input
-            type=""
-            label="એડમિશન લીધાની તારીખ"
-            name="admissionDate"
-            className="mt-8 w-[30%] mb-8 text-gray-900  sm:text-sm sm:leading-6"
-            placeholder={student.admissionDate}
-          />
-          <Input
-            type="text"
-            label="અટક"
-            name="firstname"
-            className="block w-[20%] mb-8 text-gray-900  sm:text-sm sm:leading-6"
-            placeholder={student.firstname}
-          />
-          <Input
-            type="text"
-            label="નામ"
-            name="lastname"
-            className="block w-[20%] mb-8 text-gray-900 sm:text-sm sm:leading-6"
-            placeholder={student.lastname}
-          />
-          <Input
-            type="text"
-            label="પિતાનું નામ"
-            name="fathername"
-            className="block w-[20%] mb-8 text-gray-900 sm:text-sm sm:leading-6"
-            placeholder={student.fathername}
-          />
-          <Input
-            type="text"
-            label="માતાનું નામ"
-            name="mothername"
-            className="block w-[20%] mb-8 text-gray-900 sm:text-sm sm:leading-6"
-            placeholder={student.mothername}
-          />
-          <Textarea
-            label="સરનામું"
-            name="address"
-            className=" w-[90%]"
-            placeholder={student.address}
-          />
-          <Input
-            type=""
-            label="શહેર/ગામનું નામ"
-            name="city"
-            className="mt-8 w-[30%]"
-            placeholder={student.city}
-          />
-          <Input
-            type=""
-            label="રાજ્યનું નામ"
-            name="state"
-            className="mt-8 w-[30%]"
-            placeholder={student.state}
-          />
-          <Input
-            type=""
-            label="પીનકોડ"
-            name="pincode"
-            className="mt-8 w-[30%]"
-            placeholder={student.pincode}
-          />
-          <Input
-            type=""
-            label="મોબાઈલ નંબર"
-            name="contact"
-            className="mt-8 w-[20%]"
-            placeholder={student.contact}
-          />
-          <Input
-            type=""
-            label="વાલીનો મોબાઈલ નંબર"
-            name="ParentContact"
-            className="mt-8 w-[20%]"
-            placeholder={student.parentContact}
-          />
-          <Input
-            type=""
-            label="WhatsApp નંબર"
-            name="whContact"
-            className="mt-8 w-[20%]"
-            placeholder={student.whContact}
-          />
-          <Input
-            type=""
-            label="પાસવર્ડ"
-            name="password"
-            className="mt-8 w-[20%]"
-            placeholder={student.password}
-          />
-          <Input
-            type=""
-            label="જન્મ તારીખ"
-            name="dob"
-            className="mt-8 w-[30%]"
-            placeholder={student.dob}
-          />
-          <Input
-            type=""
-            label="બ્લડ ગ્રુપ"
-            name="bloodGroup"
-            className="mt-8 w-[30%]"
-            placeholder={student.bloodGroup}
-          />
-          <Input
-            type=""
-            label="છેલ્લે અભ્યાસ કરેલ શાળા"
-            name="lastSchool"
-            className="mt-8 w-[30%]"
-            placeholder={student.lastSchool}
-          />
-          <select
-            name="isSatsangi"
-            id="isSatsangi"
-            className="block w-[20%] mt-8 text-gray-900"
-            label="સત્સંગી છે?"
-          >
-            <option value={false}>સત્સંગી છે?</option>
-            <option value={true} selected={student.isSatsangi}>
-              હા
-            </option>
-            <option value={false} selected={student.isSatsangi}>
-              ના
-            </option>
-          </select>
-          <Input
-            type="text"
-            label="નજીકનું મંદિર"
-            name="nearTemple"
-            className="mt-8 w-[30%]"
-            placeholder={student.nearTemple}
-          />
-          <Input
-            type="text"
-            label="ફીની રકમ"
-            name="fees"
-            className="mt-8 w-[30%]"
-            placeholder={student.fees}
-          />
-          <select
-            name="standard"
-            id="standard"
-            className="w-[20%] mt-8 ml-3 text-gray-900"
-          >
-            <option>વર્ગ</option>
-            <option value="7" selected={student.standard}>
-              7
-            </option>
-            <option value="11 Science" selected={student.standard}>
-              11 Science
-            </option>
-          </select>
-          <select
-            name="hostel"
-            id="hostel"
-            className="w-[20%] mt-8 text-gray-900"
-          >
-            <option value="">હોસ્ટેલ</option>
-            <option value="Shreeji Nivas" selected={student.hostel}>
-              Shreeji Nivas
-            </option>
-            <option value="Shree Hari Nivas" selected={student.hostel}>
-              Shree Hari Nivas
-            </option>
-          </select>
-          <select
-            name="hostelRoom"
-            id="hostelRoom"
-            className="w-[20%] mt-7 text-gray-900"
-          >
-            <option value={0}>હોસ્ટેલ રૂમ</option>
-            <option value="1" selected={student.hostelRoom}>
-              1
-            </option>
-            <option value="2" selected={student.hostelRoom}>
-              2
-            </option>
-          </select>
-          <Input
-            type="text"
-            label="કોઈ એલર્જી?"
-            name="alergy"
-            className="block w-[20%]  mt-8 text-gray-900  sm:text-sm sm:leading-6"
-            placeholder={student.alergy}
-          />
-          <Input
-            type="text"
-            label="કોઈ રોગ?"
-            name="dieces"
-            className="block w-[20%]  mt-8 text-gray-900 sm:text-sm sm:leading-6"
-            placeholder={student.dieces}
-          />
-          <select
-            name="isDish"
-            id="isDish"
-            className="block w-[20%] mt-8 text-gray-900"
-          >
-            <option>ડીશ લીધેલ છે?</option>
-            <option value={true} selected={student.isDish}>
-              હા
-            </option>
-            <option value={false} selected={student.isDish}>
-              ના
-            </option>
-          </select>
-          <select
-            name="isBed,"
-            id="isBed,"
-            className="block w-[20%] mt-8 text-gray-900"
-          >
-            <option>બેડિંગ લીધેલ છે?</option>
-            <option value={true} selected={student.isBed}>
-              હા
-            </option>
-            <option value={false} selected={student.isBed}>
-              ના
-            </option>
-          </select>
-          <Input label="પ્રોફાઈલ ફોટો" className=" w-[90%] mt-5" name="img" />
-          <Button type="submit" color="primary" className="mt-8 w-[50%]">
-            Update Student
-          </Button>
+          <input type="hidden" name="id" value={student.id} />
+          <div className="">
+            <label htmlFor="studentID" className="mr-3">
+              વિદ્યાર્થીનો યુનિક આઈડી:
+            </label>
+            <input
+              type="text"
+              name="studentID"
+              placeholder={student.studentID}
+              className="w-32"
+            />
+            <label htmlFor="grNumber" className="ml-3 mr-3">
+              જી.આર. નંબર:
+            </label>
+            <input
+              type="text"
+              name="grNumber"
+              placeholder={student.grNumber}
+              className="w-32"
+            />
+            <label htmlFor="admissionDate" className="ml-3 mr-3">
+              એડમિશન લીધાની તારીખ:
+            </label>
+            <input
+              type="text"
+              name="admissionDate"
+              placeholder={student.admissionDate}
+              className="w-32"
+            />
+          </div>
+          <div>
+            <label htmlFor="firstname" className="mr-3">
+              નામ:
+            </label>
+            <input
+              type="text"
+              name="firstname"
+              placeholder={student.firstname}
+              className="w-32"
+            />
+            <label htmlFor="lastname" className="ml-3 mr-3">
+              અટક:
+            </label>
+            <input
+              type="text"
+              name="lastname"
+              placeholder={student.lastname}
+              className="w-32"
+            />
+            <label htmlFor="fathername" className="ml-3 mr-3">
+              પિતાનું નામ:
+            </label>
+            <input
+              type="text"
+              name="fathername"
+              placeholder={student.fathername}
+              className="w-32"
+            />
+            <label htmlFor="mothername" className="ml-3 mr-3">
+              માતાનું નામ:
+            </label>
+            <input
+              type="text"
+              name="mothername"
+              placeholder={student.mothername}
+              className="w-32"
+            />
+          </div>
+          <div>
+            <label htmlFor="address" className="mr-3">
+              સરનામું:
+            </label>
+            <input
+              type="text"
+              name="address"
+              placeholder={student.address}
+              className="w-[70%]"
+            />
+          </div>
+          <div>
+            <label htmlFor="city" className="mr-3">
+              શહેર/ગામનું નામ:
+            </label>
+            <input
+              type="text"
+              name="city"
+              placeholder={student.city}
+              className="w-32"
+            />
+            <label htmlFor="state" className="ml-3 mr-3">
+              રાજ્યનું નામ:
+            </label>
+            <input
+              type="text"
+              name="state"
+              placeholder={student.state}
+              className="w-32"
+            />
+            <label htmlFor="pincode" className="ml-3 mr-3">
+              પીનકોડ:
+            </label>
+            <input
+              type="text"
+              name="pincode"
+              placeholder={student.pincode}
+              className="w-32"
+            />
+          </div>
+          <div>
+            <label htmlFor="contact" className="mr-3">
+              મોબાઈલ નંબર:
+            </label>
+            <input
+              type="text"
+              name="contact"
+              placeholder={student.contact}
+              className="w-32"
+            />
+            <label htmlFor="ParentContact" className="ml-3 mr-3">
+              વાલીનો મોબાઈલ નંબર:
+            </label>
+            <input
+              type="text"
+              name="ParentContact"
+              placeholder={student.ParentContact}
+              className="w-32"
+            />
+            <label htmlFor="whContact" className="ml-3 mr-3">
+              WhatsApp નંબર:
+            </label>
+            <input
+              type="text"
+              name="whContact"
+              placeholder={student.whContact}
+              className="w-32"
+            />
+          </div>
+          <div>
+            <label htmlFor="dob" className="mr-3">
+              જન્મ તારીખ:
+            </label>
+            <input
+              type="text"
+              name="dob"
+              placeholder={student.dob}
+              className="w-32"
+            />
+            <label htmlFor="bloodGroup" className="ml-3 mr-3">
+              બ્લડ ગ્રુપ:
+            </label>
+            <input
+              type="text"
+              name="bloodGroup"
+              placeholder={student.bloodGroup}
+              className="w-32"
+            />
+            <label htmlFor="lastSchool" className="ml-3 mr-3">
+              છેલ્લે અભ્યાસ કરેલ શાળા:
+            </label>
+            <input
+              type="text"
+              name="lastSchool"
+              placeholder={student.lastSchool}
+              className="w-40"
+            />
+          </div>
+          <div>
+            <label htmlFor="standard" className="mr-3">
+              વર્ગ:
+            </label>
+            <input
+              type="text"
+              name="standard"
+              placeholder={student.standard}
+              className="w-32"
+            />
+            <label htmlFor="hostel" className="ml-3 mr-3">
+              હોસ્ટેલ:
+            </label>
+            <input
+              type="text"
+              name="hostel"
+              placeholder={student.hostel}
+              className="w-40"
+            />
+            <label htmlFor="hostelRoom" className="ml-3 mr-3">
+              હોસ્ટેલ રૂમ:
+            </label>
+            <input
+              type="text"
+              name="hostelRoom"
+              placeholder={student.hostelRoom}
+              className="w-40"
+            />
+          </div>
+          <div>
+            <label htmlFor="alergy" className="mr-3">
+              કોઈ એલર્જી?:
+            </label>
+            <input
+              type="text"
+              name="alergy"
+              placeholder={student.alergy}
+              className="w-32"
+            />
+            <label htmlFor="dieces" className="ml-3 mr-3">
+              કોઈ રોગ?:
+            </label>
+            <input
+              type="text"
+              name="dieces"
+              placeholder={student.dieces}
+              className="w-40"
+            />
+          </div>
+          <div>
+            <label htmlFor="img" className="mr-3">
+              પ્રોફાઈલ ફોટો:
+            </label>
+            <input
+              type="text"
+              name="img"
+              placeholder={student.img}
+              className="w-[70%]"
+            />
+          </div>
+          <button>Update Student</button>
         </form>
       </div>
     </div>
